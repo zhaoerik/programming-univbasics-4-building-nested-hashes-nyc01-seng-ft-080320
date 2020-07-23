f5c028320f202ea304f6c8e8f67b39aeb6547d87
Erik Zhao <erikzhao98@gmail.com>
2020-07-23 00:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,27 +4,27 @@ def base_hash
 railroads => {}
 railroads
 end
-
-def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
-end
-
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
-end
-
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
-end
+# 
+# def monopoly_with_second_tier
+#   # When you start writing the implementation for this method, copy the Hash
+#   # you built in the previous method. Run the tests. They will fail, but
+#   # they'll guide you in how to modify what you just did to have more
+#   # complexity.
+# 
+# end
+# 
+# def monopoly_with_third_tier
+#   # When you start writing the implementation for this method, copy the Hash
+#   # you built in the previous method. Run the tests. They will fail, but
+#   # they'll guide you in how to modify what you just did to have more
+#   # complexity.
+# 
+# end
+# 
+# def monopoly_with_fourth_tier
+#   # When you start writing the implementation for this method, copy the Hash
+#   # you built in the previous method. Run the tests. They will fail, but
+#   # they'll guide you in how to modify what you just did to have more
+#   # complexity.
+# 
+# end
