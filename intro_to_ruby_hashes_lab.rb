d72efdaa42c98791dd00d5d5f6d537d5ca93fe7b
Erik Zhao <erikzhao98@gmail.com>
2020-07-23 01:00:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => {
-      :pieces => "4",
+      :pieces => 4,
       :rent_in_dollars => {},
       :names => {},
     },
