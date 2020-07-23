331c71ca0da26fe7401aa220040ecd5de4da4d1e
Erik Zhao <erikzhao98@gmail.com>
2020-07-23 00:55:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-railroads => {}
-railroads
+  railroads => {}
+  railroads
 end
 #
 # def monopoly_with_second_tier
